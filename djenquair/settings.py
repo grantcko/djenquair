97804c10a5b4dfe8803316327d11f29f1e4471a5
Grant Hall <grantclone@gmail.com>
2023-04-02 19:33:43 +0900

    changed views

--- djenquair/settings.py
+++ djenquair/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = ['enquair.herokuapp.com']
 # Application definition
 
 INSTALLED_APPS = [
+    'polls.apps.PollsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
